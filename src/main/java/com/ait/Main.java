e65dd801b9eca0f1df7bb119bdcc7cbeb7f038d5
venkat <venkatkilari5@gmail.com>
2023-03-23 21:23:01 +0530

    intial commit

---
+++ src/main/java/com/ait/Main.java
@@ -0,0 +1,13 @@
+package com.ait;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hello java...");
+
+	}
+
+}
